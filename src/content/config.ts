b0353129108df78994a57611b37052a62f1f4f32
Vishal Kumar Shaw <vishalshaw1221@gmail.com>
2025-11-26 22:42:15 +0530

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.date(),
     heroImage: z.string().optional(),
     math: z.boolean().optional().default(false),
+    order: z.number().optional(),
   }),
 });
 
